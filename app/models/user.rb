b19608978b150f70e1114c46eaa6747369fb82b3
azmamza <87520471+azmamza@users.noreply.github.com>
2021-08-18 16:53:31 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
 
-  has_many :items
-  has_many :orders
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i } do
+    validates :password
+    validates :password_confirmation
+  end
+
+  # has_many :items
+  # has_many :orders
 end
