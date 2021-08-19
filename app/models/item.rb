311a95a550a444d181a0aa0c632f1cf6ce77bb04
azmamza <87520471+azmamza@users.noreply.github.com>
2021-08-20 06:36:12 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
     validates :shipping_fee
     validates :prefecture
     validates :scheduled_delivery
-    validates :price, format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :image
   end
 
