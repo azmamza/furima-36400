e0ec3817c1717ccd17ba5eb5410c05113669b3c2
azmamza <87520471+azmamza@users.noreply.github.com>
2021-08-20 05:58:59 +0900

    販売手数料等の計算の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,36 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, except: [:index]
+
   def index
-    
+    @items = Item.includes(:user)
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, :price)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+  
 end
+
+# .merge(user_id: current_user.id)
