69a46d94a9079f7ba5cb5b1b6eb3fdc2a0434a66
azmamza <87520471+azmamza@users.noreply.github.com>
2021-08-29 12:16:33 +0900

    ordersコントローラーにアクション、パラメーターを定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,18 +16,19 @@ class OrdersController < ApplicationController
     end
   end
 
-
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @items.price,  # 商品の値段
-      card: order_params[:token],    # カードトークン
+      card: order_params[:token], # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end
